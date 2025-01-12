bf7c42307142ac8cc5840ab580d542e96ef60af9
Shadi Al-nabulsi <alnabulsi.shadi2019@yahoo.com>
2025-01-12 16:30:30 +0300

    adding crud operation for managing the tasks [by ammar]

---
+++ TaskManager.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class TaskManager {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+    public static void createTask(Task task) {
+        tasks.add(task);
+        System.out.println("Task created: " + task);
+    }
+
+    public static void deleteTask(Task task) {
+        if (tasks.remove(task)) {
+            System.out.println("Task deleted: " + task);
+        } else {
+            System.out.println("Task not found: " + task);
+        }
+    }
+
+    public static void updateTask(int index, Task updatedTask) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.set(index, updatedTask);
+            System.out.println("Task updated to: " + updatedTask);
+        } else {
+            System.out.println("Task not found at index: " + index);
+        }
+    }
+
+}
